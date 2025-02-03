1477b911658e6fc2555fd5653913d1414cb9e5df
simonwinbladnti <152278872+simonwinbladnti@users.noreply.github.com>
2025-02-03 13:11:52 +0100

    update

--- app.rb
+++ app.rb
@@ -1,7 +1,91 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
+    configure do
+        enable :sessions
+        set :session_secret, SecureRandom.hex(64)
+    end
+
+    def logged_in?
+        session[:user_id]
+    end
+
+    def admin?
+        current_user && current_user['username'] == 'admin'
+    end
+
+    def current_user
+        @current_user ||= db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first if logged_in?
+    end
+
+    get '/login' do
+        erb :login
+    end
+
+    get '/index' do
+        erb :index
+    end
+    
+    post '/login' do
+        username = params[:username]
+        password = params[:password]
+
+        user = db.execute("SELECT * FROM users WHERE username = ?", username).first
+
+        if user && BCrypt::Password.new(user['password']) == password
+            session[:user_id] = user['id']
+            redirect '/index'
+        else
+            @error = "Invalid username or password"
+            erb :login
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/login'
+    end
+
     get '/' do
-        erb(:"index")
+        redirect('/login') unless logged_in?
+        redirect('/index')
+    end
+
+    get '/admin' do
+        redirect('/login') unless logged_in?
+        redirect('/index') unless admin? 
+
+        @users = db.execute('SELECT * FROM users')
+        erb(:"admin")
+    end 
+
+    get '/admin/users/create' do
+        redirect('/login') unless logged_in?
+        redirect('/index') unless admin?
+
+        erb(:"create_user")
+    end
+
+    post '/admin/users' do
+        redirect('/login') unless logged_in?
+        redirect('/index') unless admin?
+
+        username = params[:username]
+        password = params[:password]
+
+        password_hashed = BCrypt::Password.create(password)
+
+        db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, password_hashed])
+
+        redirect '/admin'
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,41 @@
 require 'sqlite3'
+require 'bcrypt'
+
 
 class Seeder
-
+  
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password VARCHAR(255) NOT NULL
+    )')
+  end
+
+  def self.populate_tables
+    password_hashed = BCrypt::Password.create("admin")
+    p "Storing hashed version of password to db. Clear text never saved. #{password_hashed}"
+    db.execute('INSERT INTO users (username, password) VALUES (?, ?)', ["admin", password_hashed])
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todo.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
+
+Seeder.seed!
