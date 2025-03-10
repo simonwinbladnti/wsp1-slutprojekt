205ae514da3275b342e9bfeb84e7404df0481c40
simonwinbladnti <152278872+simonwinbladnti@users.noreply.github.com>
2025-03-10 14:49:06 +0100

    w.11

--- app.rb
+++ app.rb
@@ -1,128 +1,142 @@
-class App < Sinatra::Base
+    class App < Sinatra::Base
 
-    def db
-        return @db if @db
+        def db
+            return @db if @db
 
-        @db = SQLite3::Database.new("db/todo.sqlite")
-        @db.results_as_hash = true
+            @db = SQLite3::Database.new("db/todo.sqlite")
+            @db.results_as_hash = true
 
-        return @db
-    end
-
-    configure do
-        enable :sessions
-        set :session_secret, SecureRandom.hex(64)
-    end
-
-    def logged_in?
-        session[:user_id]
-    end
-
-    def current_team?
-        if session[:team_id]
-          db.execute("SELECT * FROM teams WHERE id = ?", session[:team_id]).first
+            return @db
         end
-    end
-      
 
-    def admin?
-        current_user && current_user['username'] == 'admin'
-    end
+        configure do
+            enable :sessions
+            set :session_secret, SecureRandom.hex(64)
+        end
 
-    def current_user
-        @current_user ||= db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first if logged_in?
-    end
+        def logged_in?
+            session[:user_id]
+        end
 
-    get '/login' do
-        erb :login
-    end
-
-    get '/index' do
-        @teams = db.execute("SELECT * FROM teams")
+        def current_team?
+            if session[:team_id]
+            db.execute("SELECT * FROM teams WHERE id = ?", session[:team_id]).first
+            end
+        end
         
-        @current_team = current_team?
-      
-        erb :index
-      end
-      
 
-    post '/select_team' do
-        team_id = params[:team_id]
-    
-        session[:team_id] = team_id
-    
-        redirect '/index'
-    end
-
-    get '/calendar' do
-    
-        team_id = session[:team_id]
-        if team_id
-            @team = db.execute("SELECT * FROM teams WHERE id = ?", team_id).first
-            erb :calendar
-        else
-            redirect '/index'
+        def admin?
+            current_user && current_user['username'] == 'admin'
         end
-    end
-    
-    
-    
-    get '/team' do
-        erb :team
-    end
-    
-    post '/login' do
-        username = params[:username]
-        password = params[:password]
 
-        user = db.execute("SELECT * FROM users WHERE username = ?", username).first
+        def current_user
+            @current_user ||= db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first if logged_in?
+        end
 
-        if user && BCrypt::Password.new(user['password']) == password
-            session[:user_id] = user['id']
-            redirect '/index'
-        else
-            @error = "Invalid username or password"
+        get '/login' do
             erb :login
         end
+
+        get '/index' do
+            @teams = db.execute("SELECT * FROM teams")
+            
+            @current_team = current_team?
+        
+            erb :index
+        end
+        
+
+        post '/select_team' do
+            team_id = params[:team_id]
+        
+            session[:team_id] = team_id
+        
+            redirect '/index'
+        end
+
+        get '/calendar' do
+            team_id = session[:team_id]
+            if team_id
+                @team = db.execute("SELECT * FROM teams WHERE id = ?", team_id).first
+                @current_team = current_team?
+        
+                @calendar = db.execute("SELECT * FROM calendar WHERE team_id = ?", team_id)
+        
+                erb :calendar
+            else
+                redirect '/index'
+            end
+        end
+        
+        
+        
+        
+        get '/team' do
+            @current_team = current_team? 
+            @team = current_team?
+
+            if @team
+            @players = db.execute("SELECT * FROM players WHERE team_id = ?", @team['id'])
+            else
+            @players = [] 
+            end
+        
+            erb :team
+        end
+        
+        
+        post '/login' do
+            username = params[:username]
+            password = params[:password]
+
+            user = db.execute("SELECT * FROM users WHERE username = ?", username).first
+
+            if user && BCrypt::Password.new(user['password']) == password
+                session[:user_id] = user['id']
+                redirect '/index'
+            else
+                @error = "Invalid username or password"
+                erb :login
+            end
+        end
+
+        get '/logout' do
+            session.clear
+            redirect '/login'
+        end
+
+        get '/' do
+            redirect('/login') unless logged_in?
+            redirect('/index')
+        end
+
+        get '/admin' do
+            redirect('/login') unless logged_in?
+            redirect('/index') unless admin? 
+
+            @users = db.execute('SELECT * FROM users')
+            erb(:"admin")
+        end 
+
+        get '/admin/users/create' do
+            redirect('/login') unless logged_in?
+            redirect('/index') unless admin?
+
+            erb(:"create_user")
+        end
+
+        post '/admin/users' do
+            redirect('/login') unless logged_in?
+            redirect('/index') unless admin?
+
+            username = params[:username]
+            password = params[:password]
+
+            password_hashed = BCrypt::Password.create(password)
+
+            db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, password_hashed])
+
+            redirect '/admin'
+        end
+
     end
-
-    get '/logout' do
-        session.clear
-        redirect '/login'
-    end
-
-    get '/' do
-        redirect('/login') unless logged_in?
-        redirect('/index')
-    end
-
-    get '/admin' do
-        redirect('/login') unless logged_in?
-        redirect('/index') unless admin? 
-
-        @users = db.execute('SELECT * FROM users')
-        erb(:"admin")
-    end 
-
-    get '/admin/users/create' do
-        redirect('/login') unless logged_in?
-        redirect('/index') unless admin?
-
-        erb(:"create_user")
-    end
-
-    post '/admin/users' do
-        redirect('/login') unless logged_in?
-        redirect('/index') unless admin?
-
-        username = params[:username]
-        password = params[:password]
-
-        password_hashed = BCrypt::Password.create(password)
-
-        db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, password_hashed])
-
-        redirect '/admin'
-    end
-
-end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,7 +1,6 @@
 require 'sqlite3'
 require 'bcrypt'
 
-
 class Seeder
   
   def self.seed!
@@ -14,6 +13,7 @@ class Seeder
     db.execute('DROP TABLE IF EXISTS users')
     db.execute('DROP TABLE IF EXISTS teams')
     db.execute('DROP TABLE IF EXISTS calendar')
+    db.execute('DROP TABLE IF EXISTS players')
   end
 
   def self.create_tables
@@ -22,13 +22,13 @@ class Seeder
       username TEXT NOT NULL,
       password VARCHAR(255) NOT NULL
     )')
+    
     db.execute('CREATE TABLE teams (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
-      label VARCHAR(255) NOT NULL,
-      players VARCHAR(255) NOT NULL,
-      calendar TEXT NOT NULL
+      label VARCHAR(255) NOT NULL
     )')
+    
     db.execute('CREATE TABLE calendar (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       team_id INTEGER NOT NULL,
@@ -37,43 +37,75 @@ class Seeder
       event_location TEXT,
       FOREIGN KEY (team_id) REFERENCES teams(id)
     )')
+
+    db.execute('CREATE TABLE players (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      role TEXT NOT NULL,
+      team_id INTEGER NOT NULL,
+      FOREIGN KEY (team_id) REFERENCES teams(id)
+    )')
   end
 
   def self.populate_tables
+    # Populate users
     password_hashed = BCrypt::Password.create("admin")
     p "Storing hashed version of password to db. Clear text never saved. #{password_hashed}"
     db.execute('INSERT INTO users (username, password) VALUES (?, ?)', ["admin", password_hashed])
   
+    # Teams data
     teams = [
-      { name: "Warriors", label: "WRS", players: "Player1, Player2", calendar: {
-        "event1" => {"date" => "2025-02-17", "time" => "10:00", "location" => "Stadium"},
-        "event2" => {"date" => "2025-03-01", "time" => "15:00", "location" => "Field"}
-      }},
-      { name: "Lions", label: "LNS", players: "Player3, Player4", calendar: {
-        "event1" => {"date" => "2025-03-05", "time" => "12:00", "location" => "Arena"},
-        "event2" => {"date" => "2025-03-12", "time" => "14:00", "location" => "Ground"}
-      }},
-      { name: "Tigers", label: "TGR", players: "Player5, Player6", calendar: {
-        "event1" => {"date" => "2025-04-01", "time" => "09:00", "location" => "Park"},
-        "event2" => {"date" => "2025-04-15", "time" => "16:00", "location" => "Hall"}
-      }},
-      { name: "Eagles", label: "EGL", players: "Player7, Player8", calendar: {
-        "event1" => {"date" => "2025-05-10", "time" => "11:00", "location" => "Stadium"},
-        "event2" => {"date" => "2025-06-01", "time" => "18:00", "location" => "Ground"}
-      }},
-      { name: "Sharks", label: "SHK", players: "Player9, Player10", calendar: {
-        "event1" => {"date" => "2025-06-15", "time" => "17:00", "location" => "Sea Arena"},
-        "event2" => {"date" => "2025-07-01", "time" => "19:00", "location" => "Water Park"}
-      }}
+      { name: "Warriors", label: "WRS" },
+      { name: "Lions", label: "LNS" },
+      { name: "Tigers", label: "TGR" },
+      { name: "Eagles", label: "EGL" },
+      { name: "Sharks", label: "SHK" }
     ]
   
     teams.each do |team|
-      calendar_json = team[:calendar].to_json
-      db.execute('INSERT INTO teams (name, label, players, calendar) VALUES (?, ?, ?, ?)',
-                 [team[:name], team[:label], team[:players], calendar_json])
+      db.execute('INSERT INTO teams (name, label) VALUES (?, ?)', [team[:name], team[:label]])
     end
+    team_ids = db.execute('SELECT id, name FROM teams').to_h { |row| [row['name'], row['id']] }
+
+    players = [
+      { name: "Player1", role: "Forward", team: "Warriors" },
+      { name: "Player2", role: "Defender", team: "Warriors" },
+      { name: "Player3", role: "Forward", team: "Lions" },
+      { name: "Player4", role: "Goalkeeper", team: "Lions" },
+      { name: "Player5", role: "Midfielder", team: "Tigers" },
+      { name: "Player6", role: "Forward", team: "Tigers" },
+      { name: "Player7", role: "Defender", team: "Eagles" },
+      { name: "Player8", role: "Goalkeeper", team: "Eagles" },
+      { name: "Player9", role: "Forward", team: "Sharks" },
+      { name: "Player10", role: "Defender", team: "Sharks" }
+    ]
   
-    p "Teams have been successfully populated into the database."
+    players.each do |player|
+      team_id = team_ids[player[:team]]
+      db.execute('INSERT INTO players (name, role, team_id) VALUES (?, ?, ?)', [player[:name], player[:role], team_id])
+    end
+
+    # Calendar data for events
+    calendar = [
+      { team_name: "Warriors", date: "2025-02-17", time: "10:00", location: "Stadium" },
+      { team_name: "Warriors", date: "2025-03-01", time: "15:00", location: "Field" },
+      { team_name: "Lions", date: "2025-03-05", time: "12:00", location: "Arena" },
+      { team_name: "Lions", date: "2025-03-12", time: "14:00", location: "Ground" },
+      { team_name: "Tigers", date: "2025-04-01", time: "09:00", location: "Park" },
+      { team_name: "Tigers", date: "2025-04-15", time: "16:00", location: "Hall" },
+      { team_name: "Eagles", date: "2025-05-10", time: "11:00", location: "Stadium" },
+      { team_name: "Eagles", date: "2025-06-01", time: "18:00", location: "Ground" },
+      { team_name: "Sharks", date: "2025-06-15", time: "17:00", location: "Sea Arena" },
+      { team_name: "Sharks", date: "2025-07-01", time: "19:00", location: "Water Park" }
+    ]
+  
+    calendar.each do |event|
+      team_id = team_ids[event[:team_name]]
+      db.execute('INSERT INTO calendar (team_id, event_date, event_time, event_location) VALUES (?, ?, ?, ?)', 
+                 [team_id, event[:date], event[:time], event[:location]])
+    end
+
+    p "Database successfully populated with users, teams, players, and events."
   end
   
 
@@ -85,6 +117,5 @@ class Seeder
     @db
   end
 end
-
 
 Seeder.seed!
